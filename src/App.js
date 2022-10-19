e600569446a9b2cf84bedd49b347a2674201cac8
Akash_96 <realakash9630@gmail.com>
2022-10-19 10:52:21 +0530

    alter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
 
-function App() {
-  return (
-    <div className="App">
-      Hello akash
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { monsters: [] }
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }))
+  }
+  render() {
+    return (
+      <div className="container">
+        <CardList name="Yihua">
+          {this.state.monsters.map(monster => (
+            <h1 key={monster.id}>{monster.name}</h1>))}
+        </CardList>
+      </div>
+    );
+  }
 }
-
 export default App;
