b857159ea08278f996b26f0a6883b356187a8eea
Akash_96 <realakash9630@gmail.com>
2022-10-20 19:03:51 +0530

    Bigelow font added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list.component';
 import { SearchBox } from './components/searchbox/search-box.component';
-
+import "./App.css"
 
 class App extends Component {
   constructor() {
@@ -9,6 +9,10 @@ class App extends Component {
     this.state = { monsters: [], searchField: "" }
   }
 
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value })
+  }
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then(response => response.json())
@@ -16,14 +20,14 @@ class App extends Component {
   }
   render() {
     const { monsters, searchField } = this.state;
-    // const monsters = this.state.monsters;
-    // const searchField = this.state.searchField;
+
     const filteredMonsters = monsters.filter(monster => monster.name.toLowerCase().includes(searchField.toLowerCase()))
     return (
       <div className="App">
+        <h1>Monsters Rolodex</h1>
         <SearchBox
           placeholder="search monsters"
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList
           monsters={filteredMonsters}
