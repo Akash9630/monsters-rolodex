bf577c10c8b0d495fa8a7a6f9bda414661953541
Akash_96 <realakash9630@gmail.com>
2022-11-14 21:38:44 +0530

    alter

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -2,9 +2,13 @@ import React from "react";
 import { Card } from "../card/card.component";
 import "./card-list.style.css";
 export const CardList = (props) => (
-  <div className="card-list">
-    {props.monsters.map((monster) => (
-      <Card key={monster.id} monster={monster} />
-    ))}
+  <div className="main">
+    <div className="section">
+      <div className="card-list">
+        {props.monsters.map((monster) => (
+          <Card key={monster.id} monster={monster} />
+        ))}
+      </div>
+    </div>
   </div>
 );
--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -3,10 +3,9 @@ import "./cardstyle.css";
 export const Card = (props) => (
   <div className="card-container">
     <img
-      src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180`}
+      src={`https://robohash.org/${props.monster.id}?set=set5&size=180x180`}
       alt="not found"
     />
     <h1>{props.monster.name}</h1>
-    <p>{props.monster.email}</p>
   </div>
 );
